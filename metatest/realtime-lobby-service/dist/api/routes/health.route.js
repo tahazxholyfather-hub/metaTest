b9c984e1b350ccb54aab706bf475bfac05d78464
tahazxholyfather-hub <tahazxholyfather@gmail.com>
2026-09-01 19:52:55 +0330

    unzipgit

---
+++ metatest/realtime-lobby-service/dist/api/routes/health.route.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.healthRouter = void 0;
+const express_1 = require("express");
+exports.healthRouter = (0, express_1.Router)();
+exports.healthRouter.get('/', (_req, res) => {
+    res.json({
+        success: true,
+        service: 'realtime-lobby-backend',
+        status: 'ok',
+        timestamp: new Date().toISOString(),
+    });
+});
