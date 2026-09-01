c6daf10c545fd323bd76d8e09ccd50ef0598ee08
tahazxholyfather-hub <tahazxholyfather@gmail.com>
2026-09-01 20:25:15 +0330

    unzipgit

---
+++ metatest/src/backend/node_modules/zarinpal-node-sdk/jest.config.cjs
@@ -0,0 +1,11 @@
+// jest.config.cjs
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/tests/**/*.test.ts'],
+    moduleFileExtensions: ['ts', 'js'],
+    moduleNameMapper: {
+      '^axios$': require.resolve('axios'),
+    },
+  };
+  
